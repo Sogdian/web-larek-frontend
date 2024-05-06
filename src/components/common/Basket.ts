d5004f8b7f35bbf6f69f36c2ba296a374301b44f
sogdian <sogdian4815@gmail.com>
2024-05-06 17:43:38 +0200

    описал Basket

--- src/components/common/Basket.ts
+++ src/components/common/Basket.ts
@@ -1,22 +1,46 @@
-import { Component } from '../base/component';
-import { EventEmitter } from '../base/events';
-import { IBasket } from '../../types';
+import {Component} from '../base/component';
+import {EventEmitter} from '../base/events';
+import {IBasket} from '../../types';
+import {createElement, ensureElement} from "../../utils/utils";
 
 /**
  * Класс для работы с корзиной, наследуется от класса Component (реализация слоя View).
  * Класс используется для управления отображением данных (товаров, цены) в компоненте корзины
  */
 export class Basket extends Component<IBasket> {
+    protected list: HTMLElement;
+    protected total: HTMLElement | null;
+    protected button: HTMLButtonElement | null;
+
     constructor(container: HTMLElement, protected events: EventEmitter) {
         super(container);
+
+        this.list = ensureElement<HTMLElement>('.basket__list', this.container);
+        this.total = this.container.querySelector('.basket__price');
+        this.button = this.container.querySelector('.basket__button');
     }
 
     //установка списка товаров
-    set items(items: HTMLElement[]): void
+    set items(items: HTMLElement[]) {
+        if (items.length) {
+            this.list.replaceChildren(...items);
+        }
+        else {
+            this.list.replaceChildren(
+                createElement<HTMLParagraphElement>('p', {
+                    textContent: 'В корзине пусто',
+                })
+            );
+        }
+    }
 
     //установка цены
-    set price(value: number): void
+    set price(value: number){
+        this.total.textContent = String(value) + ' синапсов';
+    }
 
     //блокировка кнопки
-    disableButton(value: boolean): void
+    disableButton(value: boolean){
+        this.button.disabled = value;
+    }
 }
