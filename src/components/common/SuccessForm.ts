9914b025a66fb5178212f0213b67eaf7593a2ab0
sogdian <sogdian4815@gmail.com>
2024-05-06 18:12:30 +0200

    добавил SuccessForm

--- src/components/common/SuccessForm.ts
+++ src/components/common/SuccessForm.ts
@@ -1,15 +1,22 @@
 import {IOrderSuccess} from "../../types";
 import {Component} from '../base/component';
+import {ensureElement} from "../../utils/utils";
 
 /**
  * Класс для работы с окном успешного оформления заказа, наследуется от класса Component (реализация слоя View).
  * Класс используется для управления отображением данных (стоимость товара) в компоненте модального окна успешного оформления заказа
  */
 export class SuccessForm extends Component<IOrderSuccess> {
+    protected count: HTMLElement;
+
     constructor(container: HTMLElement) {
         super(container);
+
+        this.count = ensureElement<HTMLElement>('.order-success__description', this.container);
     }
 
     //установка количества списанных синапсов
-    set count(value: number): void
+    set count(value: number) {
+        this.count.textContent = '!Списано ' + String(value) + ' синапсов';
+    }
 }
