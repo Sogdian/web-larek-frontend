39b7af308cf0d397a8d2136c886d3a204fa20663
sogdian <sogdian4815@gmail.com>
2024-05-09 14:26:36 +0200

    добавил Modal

--- src/components/common/Modal.ts
+++ src/components/common/Modal.ts
@@ -7,19 +7,35 @@ import {IEvents} from "../base/events";
  * Класс используется для управления состоянием (открыт, закрыт) и отображением компонента (render) модального окна
  */
 export class Modal extends Component<IModal> {
+    protected content: HTMLElement;
+
     constructor(container: HTMLElement, protected events: IEvents) {
         super(container);
     }
 
     //установка значения в модальном окне
-    set content(value: HTMLElement): void
+    set content(value: HTMLElement) {
+        this.content.replaceChildren(value);
+    }
 
     //открытие модального окна
-    open(): void
+    open() {
+        this.container.classList.add('modal_active');
+        this.events.emit('modal:open');
+    }
 
     //закрытие модального окна
-    close(): void
+    close(){
+        this.container.classList.remove('modal_active');
+        this.content = null;
+        this.events.emit('modal:close');
+    }
 
     //отображение модального окна
-    render(data: IModal): HTMLElement
+    render(data: IModal) {
+        super.render(data);
+        this.open();
+
+        return this.container;
+    }
 }
