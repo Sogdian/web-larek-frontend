be6933035c8ea9d8f2b2f86da3d929737b67353a
sogdian <sogdian4815@gmail.com>
2024-05-09 14:25:02 +0200

    добавил Form

--- src/components/common/Form.ts
+++ src/components/common/Form.ts
@@ -7,19 +7,37 @@ import {IForm} from "../../types";
  * Класс используется для установки значения валидности и передачу ошибок в компонент, а также для отображения компонента (render) формы заполнения данных
  */
 export class Form<T> extends Component<IForm> {
+    protected submit: HTMLButtonElement;
+    protected errors: HTMLElement;
+
     constructor(protected container: HTMLFormElement, protected events: IEvents) {
         super(container);
     }
 
     //изменение поля в заказе
-    protected onInputChange(field: keyof T, value: string): void
+    protected onInputChange(field: keyof T, value: string){
+        this.events.emit(`order:change`, {
+            field,
+            value,
+        });
+    }
 
     //установка значения валидности
-    set valid(value: boolean): void
+    set valid(value: boolean){
+        this.submit.disabled = !value;
+    }
 
     //передача ошибок в форме
-    set errors(value: string): void
+    set errors(value: string){
+        this.setText(this.errors, value);
+    }
 
     //отображение формы
-    render(state: Partial<T> & IForm): void
+    render(state: Partial<T> & IForm){
+        const { valid, errors, ...inputs } = state;
+        super.render({ valid, errors });
+        Object.assign(this, inputs);
+
+        return this.container;
+    }
 }
